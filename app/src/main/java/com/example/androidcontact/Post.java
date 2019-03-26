3304893f64e509bf5fbcff5aca2758b41fa01b04
MathieuRalamboson <ralamboson@et.esiea.fr>
2019-03-26 17:08:05 +0100

    Post créé

---
+++ app/src/main/java/com/example/androidcontact/Post.java
@@ -0,0 +1,24 @@
+package com.example.androidcontact;
+
+import java.util.List;
+
+public class Post {
+
+    private int userId;
+    private String name;
+    private String userName;
+    //private List<Contact> results
+
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+}
